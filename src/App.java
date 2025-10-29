36e5dc8691c6ec374b4cb7247c620dfabb3a5e53
AdrianGF <160476564+AdriGuFe@users.noreply.github.com>
2025-10-29 13:16:20 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
 
         for (int i=2;i<=100;i++){
             esPrimo=true;
-            for (int j=2;j<i;j++){
+            for (int j=2;j<i/2;j++){
                 if (i%j==0){
                     esPrimo=false;
                 }
